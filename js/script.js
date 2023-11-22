64549c1d999edde70eeedca8d3cad1e8a8c01188
moonymel <musumeci.me@gmail.com>
2023-11-22 15:13:02 +0100

    fixed script.js - ESERCIZIO COMPLETATO

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 
 for(let i=1; i<=100; i++) {
 
-    // CREO LA MODIFICA IN CONSOLE PER I MULTIPLI DI 3
-    if(i%3 == 0) {
-        console.log('Fizz');
+    // CREO LA MODIFICA IN CONSOLE PER I MULTIPLI DI 3 E DI 5
+    if(i%3 == 0 && i%5 == 0) {
+        console.log('FizzBuzz');
     }
 
     // CREO LA MODIFICA IN CONSOLE PER I MULTIPLI DI 5
@@ -12,9 +12,9 @@ for(let i=1; i<=100; i++) {
         console.log('Buzz');
     }
 
-    // CREO LA MODIFICA IN CONSOLE PER I MULTIPLI DI 3 E DI 5
-    else if(i%3 == 0 && i%5 == 0) {
-        console.log('FizzBuzz');
+    // CREO LA MODIFICA IN CONSOLE PER I MULTIPLI DI 3
+    else if(i%3 == 0) {
+        console.log('Fizz');
     }
 
     else {
